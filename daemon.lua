9668c097b2c588f8b74bad0e4ed80d14a64efd0a
Lourival Vieira Neto <lourival.neto@ring-0.io>
2024-07-07 16:10:49 +0000

    unify http and https filters into filter.{c,lua}
    * avoid udp.connect(), use udp.sendto() instead

--- daemon.lua
+++ daemon.lua
@@ -3,39 +3,51 @@
 -- SPDX-License-Identifier: GPL-2.0-only
 --
 
+local lunatik = require("lunatik")
 local thread  = require("thread")
 local socket  = require("socket")
 local inet    = require("socket.inet")
 local linux   = require("linux")
 local mailbox = require("mailbox")
-local filter  = require("dome/filter")
+local rcu     = require("rcu") -- used for lunatik.runtimes()
+
+local runtimes = lunatik.runtimes()
 
 local shouldstop = thread.shouldstop
 
 local telegraf = inet.udp()
-telegraf:connect(inet.localhost, 8094)
+function telegraf:push(message)
+	self:sendto(message, inet.localhost, 8094)
+end
 
 local inbox = mailbox.inbox(100 * 1024)
-__filters = { -- keep refs to avoid __gc()
-	sni      = filter.new("sni", inbox.queue),
-	hostname = filter.new("hostname", inbox.queue),
-}
 
-local header = 'https,host=ring-0.io,location=rj '
+local header = 'http,host=ring-0.io,location=rj '
+
+local function dispatch(script, ...)
+	if runtimes[script] then
+		error(string.format("%s is already running", script))
+	end
+
+	local runtime = lunatik.runtime(script, false)
+	thread.run(runtime, script, ...)
+	runtimes[script] = runtime
+end
 
 local function daemon()
 	print("[ring-0/dome] started")
 	while (not shouldstop()) do
 		local message = inbox:receive()
 		if message then
-			local message = string.gsub(message, "(%u+)(%g+)", header .. 'domain="%2",action="%1"\n')
-			telegraf:send(message)
+			telegraf:push(header .. message)
 		else
 			linux.schedule(100)
 		end
 	end
 	print("[ring-0/dome] stopped")
 end
+
+dispatch("dome/filter", inbox.queue)
 
 return daemon
 
--- filter.lua
+++ filter.lua
@@ -3,33 +3,108 @@
 -- SPDX-License-Identifier: GPL-2.0-only
 --
 
-local lunatik = require("lunatik")
-local thread  = require("thread")
-local rcu     = require("rcu") -- used for lunatik.runtimes()
+local linux   = require("linux")
+local xdp     = require("xdp")
+local mailbox = require("mailbox")
+local malware = require("dome/malware")
 
-local runtimes = lunatik.runtimes()
+local policy = "PASS"
 
-local filter = {}
-
-function filter.__gc(self)
-	self.runtime:stop()
+local format = string.format
+local function log(fmt, ...)
+	print(format("[ring-0/dome] " .. fmt, ...))
 end
 
-function filter.new(name, queue)
-	local name = 'dome/' .. name
-
-	if runtimes[name] then
-		error(string.format("%s is already running", name))
+local ntoh16 = linux.ntoh16
+local function unpacker(packet, base)
+	local u8 = function (offset)
+		return packet:getuint8(base + offset)
 	end
 
-	local runtime = lunatik.runtime(name, false)
-	runtimes[name] = runtime 
+	local be16 = function (offset)
+		return ntoh16(packet:getuint16(base + offset))
+	end
 
-	-- dispatch queue to filter
-	thread.run(runtime, name, queue)
+	local str = function (offset, length)
+		return packet:getstring(base + offset, length)
+	end
 
-	return setmetatable({runtime = runtime}, filter)
+	return str, u8, be16
 end
 
-return filter
+local function hostname(packet, offset, length)
+	local str = unpacker(packet, offset)
+	local request = str(0, length)
+	return string.match(request, "Host:%s(.-)\r\n")
+end
+
+local function sni(packet, offset)
+	local str, u8, be16 = unpacker(packet, offset)
+
+	local client_hello = 0x01
+	local handshake    = 0x16
+	local server_name  = 0x00
+
+	local session = 43
+	local max_extensions = 17
+
+	if u8(0) ~= handshake or u8(5) ~= client_hello then
+		return
+	end
+
+	local cipher = (session + 1) + u8(session)
+	local compression = cipher + 2 + be16(cipher)
+	local extension = compression + 3 + u8(compression)
+
+	for i = 1, max_extensions do
+		local data = extension + 4
+		if be16(extension) == server_name then
+			local length = be16(data + 3)
+			return str(data + 5, length)
+		end
+		extension = data + be16(extension + 2)
+	end
+end
+
+local parsers = {
+	[ 80] = hostname,
+	[443] = sni,
+}
+
+local function filter(outbox)
+	local action = xdp.action
+
+	local function argparse(argument)
+		return function (n)
+			return argument:getuint16((n - 1) * 2)
+		end
+	end
+
+	return function (packet, argument)
+		local arg = argparse(argument)
+		local dport, offset, length = arg(1), arg(2), arg(3)
+		local parser = parsers[dport]
+
+		if not parser then
+			log("filter was not found (dport = %d)", dport)
+		else
+			local verdict = policy
+			local domain  = parser(packet, offset, length)
+			if domain then
+				verdict = malware[domain] and "DROP" or verdict
+				outbox:send(format('domain="%s",dport="%d",action="%s"', domain, dport, verdict))
+			end
+			return action[verdict]
+		end
+	end
+end
+
+local function attacher(queue)
+	local outbox = mailbox.outbox(queue)
+	xdp.attach(filter(outbox))
+	log("filter attached")
+end
+
+log("filter loaded")
+return attacher
 
