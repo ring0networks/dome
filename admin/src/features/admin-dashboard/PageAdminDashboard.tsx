9a5f73447153929fc91373c9226a649c2ae9adb3
Mauricio Bomfim <mauriciobomfim@gmail.com>
2024-11-18 22:40:12 -0300

    Make Grafana compatible with Admin interface

--- admin/src/features/admin-dashboard/PageAdminDashboard.tsx
+++ admin/src/features/admin-dashboard/PageAdminDashboard.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function PageAdminDashboard() {
   return (
     <>
-      <iframe src="http://localhost:3000/d/edr5k61vs5hj4d/ring-0-d0me?orgId=1&refresh=5s" height="1000px" />
+      <iframe src="http://localhost:3000/d/abc5k61vs5hj4d/ring-0-d0me?orgId=1&refresh=5s" height="1000px" />
     </>
   );
 }
